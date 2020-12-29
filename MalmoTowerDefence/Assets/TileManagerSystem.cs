6a1bd9359cf2235f41f620750a78d8defcf6b4c3
Jonatan-Catalan <jonatanacatalan@hotmail.com>
2020-12-29 12:15:50 +0100

    TileManager

---
+++ MalmoTowerDefence/Assets/TileManagerSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileManagerSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject Tile;
+
+    [SerializeField] private static float rowDistance = 1.5f;
+    [SerializeField] private static float columnDistance = 1.5f;
+
+    public GameObject[] lanes = new GameObject[5];
+    
+    
+}
